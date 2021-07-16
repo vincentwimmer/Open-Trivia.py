d02e33edfc05aa60b21f70e3acc1cf95d1ac6a85
vincentwimmer <vincentwimmer@gmail.com>
2021-07-16 11:56:58 -0400

    Added Score Keeping
    Keeps score by storing it in a txt file.
    Also added the correct answer to the fail message.

--- open-trivia.py
+++ open-trivia.py
@@ -3,9 +3,16 @@ import json
 import requests
 import html
 
+
+scoreFilePath = "C:/Path/To/score.txt"
+#scoreFilePath = "score.txt"
+
 rounds = 3
 
 for qnum in range(rounds):
+	input_file = open(scoreFilePath, "r")
+	pScore = int(input_file.read())
+
 	qnum = qnum + 1
 	getQ = requests.get("https://opentdb.com/api.php?amount=1")
 	getQ = json.loads(getQ.text)['results'][0]
@@ -39,15 +46,31 @@ for qnum in range(rounds):
 	print(strQ)
 
 	while True:
+		nScore = 0
 		val = input("Enter your value: ")
 		val = val.lower()
-
 		try:
 			if ansDict[val] == getQ['correct_answer']:
 				print("Correct!")
+
+				if (str(getQ['difficulty']).lower()) == "easy":
+					nScore = 10
+				if (str(getQ['difficulty']).lower()) == "medium":
+					nScore = 25
+				if (str(getQ['difficulty']).lower()) == "hard":
+					nScore = 50
+
+				pScore = pScore + nScore
+
+				input_file = open(scoreFilePath, "w")
+				input_file.write(str(pScore))
+				input_file.close()
+
+				print("Question Points", nScore)
+				print("Total Points", pScore)
 				break
 			else:
-				print("Nope!")
+				print("Sorry the correct answer was:", str(getQ['correct_answer']))
 				break
 		except:
 			print("Incorrect Input!")
